ad2b936aac57234f1ad6c52a0e65b2b40b3cad5d
Gil <gil.chen@vpon.com>
2016-10-21 01:55:25 +0800

    update desc

--- build_spark_on_EC2.sh
+++ build_spark_on_EC2.sh
@@ -1,5 +1,6 @@
+# this script will build spark on EC2
 
-# download spark
+# download spark(not pre-build version)
 wget http://d3kbcqa49mib13.cloudfront.net/spark-2.0.1.tgz
 
 # decompress
@@ -8,23 +9,26 @@ tar zxvf spark-2.0.1.tgz
 # move to right path
 sudo mv spark-2.0.1 /opt/spark/
 
-# build/mvn  這步會跑大概 30 分鐘
+# Build with Hive (maybe spent 30 mins)
 ./build/mvn -Pyarn -Phadoop-2.7 -Dhadoop.version=2.7.2 -Phive -Phive-thriftserver -DskipTests clean package
 
-# config
+# Configure Hive Connectio: add hive-site.xml
 cp /opt/hive/apache-hive-1.2.1-bin/conf/hive-site.xml /opt/spark/spark-2.0.1/conf
 
-echo "spark.driver.extraClassPath /opt/hive/apache-hive-1.2.1-bin/lib/mysql-connector-java-5.1.38-bin.jar" >> /opt/spark/spark-2.0.1/conf/spark-defaults.conf.template
+# mysql-connector for Hive metastore
+echo "spark.driver.extraClassPath /opt/hive/apache-hive-1.2.1-bin/lib/mysql-connector-java-5.1.38-bin.jar" >> /opt/spark/spark-2.0.1/conf/spark-defaults.conf
 
+# Configure s3 file system: add core-site.xml
 cp /opt/hadoop/hadoop-2.7.2/etc/hadoop/core-site.xml /opt/spark/spark-2.0.1/conf
 
-echo "spark.driver.extraClassPath /opt/hadoop/hadoop-2.7.2/share/hadoop/tools/lib/aws-java-sdk-1.7.4.jar:/opt/hadoop/hadoop-2.7.2/share/hadoop/tools/lib/hadoop-aws-2.7.2.jar" >> /opt/spark/spark-2.0.1/conf/spark-defaults.conf.template
+# aws connector for s3
+echo "spark.driver.extraClassPath /opt/hadoop/hadoop-2.7.2/share/hadoop/tools/lib/aws-java-sdk-1.7.4.jar:/opt/hadoop/hadoop-2.7.2/share/hadoop/tools/lib/hadoop-aws-2.7.2.jar" >> /opt/spark/spark-2.0.1/conf/spark-defaults.conf
 
 
 sudo su -c "R -e \"install.packages('testthat', repos='https://cran.rstudio.com/')\""
 
 
-
+# example
 rm(list=ls())
 Sys.setenv(SPARK_HOME="/opt/spark/spark-2.0.1/")
 .libPaths(c(file.path(Sys.getenv("SPARK_HOME"), "R", "lib"), .libPaths()))
